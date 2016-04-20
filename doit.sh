019a4662ab6da0d5ca613d4235c179f634f87f48
a-tal <github@talsma.ca>
2016-04-20 09:35:05 +0000

    adds doitlive script and kube config w/out configs/secrets

---
+++ doit.sh
@@ -0,0 +1,19 @@
+mv ~/Downloads/Eve*.json .
+mv Eve*.json key.json
+ls
+kubectl create secret generic shiptoasting-gcloud-secret --from-file=key.json
+kubectl get secrets
+cat sso-config.json | grep -v secret
+kubectl create secret generic shiptoasting-sso-secret --from-file=sso-config.json
+kubectl get secrets
+kubectl get secret shiptoasting-sso-secret -o yaml | grep sso-config.json | cut -d ':' -f2 | sed 's/^ *//' | base64 -D | grep -v secret
+kubectl create configmap shiptoasting-nginx-config --from-file=$(pwd)/nginx
+kubectl get configmap
+kubectl get configmap shiptoasting-nginx-config -o yaml
+random_string 256
+kubectl create secret generic shiptoasting-flask-app --from-literal=secret=$(random_string 512)
+kubectl create secret generic ccp-favicon --from-file=favicon.ico
+kubectl create secret generic tech-ccp-is-certs --from-file=$(pwd)/ssl
+kubectl cluster-info
+kubectl config view | grep -A4 $(kubectl config current-context) | grep password | cut -d ':' -f2 | sed 's/^ *//' | pbcopy
+echo "done"
